8e6fb158848f99c993567be9dd39271ffb96efa6
EdoardoTosin <877438@stud.unive.it>
2021-03-04 15:54:39 +0100

    Create zoom-web-client-redirector.js

---
+++ zoom-web-client-redirector.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Zoom Web Client Redirector
+// @namespace    https://zoom.us
+// @version      1.0
+// @description  Redirect Zoom meetings to the web client version
+// @author       Edoardo Tosin
+// @include      /https:\/\/([a-z0-9\-]+\.)?zoom\.us\/([j])\/([0-9]+).*/
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    const link = window.location.href.match(/https:\/\/([a-z0-9\-]+\.)?zoom\.us\/([j])\/([0-9]+).*/);
+    const prefix = link[1] || "";
+    const postfix = link[2] === "j" ? "join/" + link[3] : link[3] + "/start";
+    window.location.href = "https://" + prefix + "zoom.us/wc/" + postfix;
+})();
