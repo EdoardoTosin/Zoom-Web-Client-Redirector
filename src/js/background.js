b49eda08148089c1e95d4c477ba7a9ac55a3ef13
EdoardoTosin <877438@stud.unive.it>
2021-03-08 20:16:20 +0100

    Fix js toggle checkbox

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,7 @@
 
 //Replace */j/* on zoom domain with */wc/join/*
 (function redirect() {
-    if (window.location.pathname.substring(0,3) == "/j/") {
+    if (window.location.pathname.substring(0,3) == "/j/" && document.getElementById('toggle')) {
       const domain = window.location.hostname;
       const path = "/wc/join/" + window.location.pathname.substring(3);
       window.location.href = "https://" + domain + path;
