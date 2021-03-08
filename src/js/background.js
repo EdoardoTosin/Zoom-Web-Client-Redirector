57dbf481eb7017a867b29746842cbfcb0fda52a9
EdoardoTosin <877438@stud.unive.it>
2021-03-08 23:43:53 +0100

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,25 @@
 
+
+if (localStorage.getItem('toggleStatus') != true && localStorage.getItem('toggleStatus') != false) {
+  localStorage.setItem("toggleStatus", true);
+}
+var checkbox = document.getElementById('toggle');
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    checkbox.checked = true;
+    localStorage.setItem("toggleStatus", true);
+    this.setAttribute("checked", "checked");
+  }
+  else {
+    checkbox.checked = false;
+    localStorage.setItem("toggleStatus", false);
+    this.removeAttribute("checked");
+  }
+})();
+if (localStorage.getItem('toggleStatus') == true){
+  redirect();
+}
+
 //Replace */j/* on zoom domain with */wc/join/*
 (function redirect() {
     if (window.location.pathname.substring(0,3) == "/j/") {
