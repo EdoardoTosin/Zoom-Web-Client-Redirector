a1025fbe7bcf9ee0084ac4832f1459bc08f0ff4f
EdoardoTosin <877438@stud.unive.it>
2021-03-09 17:27:42 +0100

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -18,16 +18,31 @@ function changeStatus() {
 function changeState(){
   document.getElementById('toggle').addEventListener("change", changeStatus());
 };
-
+/*
 if (localStorage.getItem('toggleStatus') == true){
   redirect();
-}
-
+};
+*/
 //Replace */j/* on zoom domain with */wc/join/*
 (function redirect(){
-    if (window.location.pathname.substring(0,3) == "/j/") {
+    if (window.location.pathname.substring(0,3) == "/j/" && localStorage.getItem('toggleStatus') != false) {
       const domain = window.location.hostname;
       const path = "/wc/join/" + window.location.pathname.substring(3);
       window.location.href = "https://" + domain + path;
     }
 })();
+
+function changeLocal(){
+  if (localStorage.getItem("toggleStatus") != false){
+    localStorage.setItem("toggleStatus", true);
+    document.getElementById('toggle').setAttribute("checked","");
+  }
+  else {
+    document.getElementById('toggle').removeAttribute("checked");
+  }
+};
+
+window.addEventListener("load", function(event) {
+    console.log("Tutte le risorse hanno terminato il caricamento!");
+    changeLocal();
+});
