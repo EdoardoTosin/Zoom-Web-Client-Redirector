86ab23e826441734cb6ec5df08dca12f3858c70f
EdoardoTosin <877438@stud.unive.it>
2021-03-09 11:21:33 +0100

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -29,7 +29,7 @@ if (localStorage.getItem('toggleStatus') == true){
 
 //Replace */j/* on zoom domain with */wc/join/*
 (function redirect(){
-    if (window.location.pathname.substring(0,3) == "/j/" && localStorage.getItem('toggleStatus') == true) {
+    if (window.location.pathname.substring(0,3) == "/j/") {
       const domain = window.location.hostname;
       const path = "/wc/join/" + window.location.pathname.substring(3);
       window.location.href = "https://" + domain + path;
