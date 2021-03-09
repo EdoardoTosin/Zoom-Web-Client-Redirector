e7464c7edfe450d4908d54ec12f0cd02ef84747f
EdoardoTosin <877438@stud.unive.it>
2021-03-09 11:10:41 +0100

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,29 +1,34 @@
 
 
-if (localStorage.getItem('toggleStatus') != true && localStorage.getItem('toggleStatus') != false) {
+if (localStorage.getItem('toggleStatus') == false){
+  localStorage.setItem("toggleStatus", false);
+  document.getElementById('toggle').removeAttribute("checked");
+}
+else if (localStorage.getItem('toggleStatus') != false){
   localStorage.setItem("toggleStatus", true);
+  document.getElementById('toggle').setAttribute("checked", "");
 }
 
 var checkbox = document.getElementById('toggle');
-checkbox.addEventListener('change', function() {
-  if (this.checked) {
-    checkbox.checked = true;
+checkbox.addEventListener("change", function(){
+  if (this.checked){
+    this.checked = true;
     localStorage.setItem("toggleStatus", true);
-    this.setAttribute("checked", "checked");
+    this.setAttribute("checked", "");
   }
-  else {
-    checkbox.checked = false;
+  else{
+    this.checked = false;
     localStorage.setItem("toggleStatus", false);
     this.removeAttribute("checked");
   }
-})();
+});
 
 if (localStorage.getItem('toggleStatus') == true){
   redirect();
 }
 
 //Replace */j/* on zoom domain with */wc/join/*
-(function redirect() {
+(function redirect(){
     if (window.location.pathname.substring(0,3) == "/j/") {
       const domain = window.location.hostname;
       const path = "/wc/join/" + window.location.pathname.substring(3);
