662f4f8075a405e75b18c0b6d54e8164d8a73671
EdoardoTosin <877438@stud.unive.it>
2021-03-12 00:08:30 +0100

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -90,7 +90,7 @@ function setOffIcon(){
 
 // Replace */j/* on zoom domain with */wc/join/*
 (function redirect(){
-  if (/*localStorage.getItem("toggleStatus")=="true" && */window.location.pathname != null && window.location.pathname.substring(0,3) == "/j/") {
+  if (/*window.localStorage.getItem("toggleStatus")=="true" && */window.location.pathname != null && window.location.pathname.substring(0,3) == "/j/") {
     const domain = window.location.hostname;
     const path = "/wc/join/" + window.location.pathname.substring(3);
     window.location.href = "https://" + domain + path;
