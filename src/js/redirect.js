1176aa0e89101f6e00a023f6d0b7b08a42b60a1a
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2021-05-12 23:19:35 +0200

    fixed runtime.error incompatible with Firefox

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,13 +1,11 @@
 // Replace */j/* on zoom domain with */wc/join/*
 (function redirect(){
   chrome.storage.sync.get("toggle", function(items) {
-    if (!chrome.runtime.error) {
-      console.log(items.toggle);
+      //console.log(items.toggle);
       if (items.toggle=="true" && window.location.pathname != null && window.location.pathname.substring(0,3) == "/j/"){
         const domain = window.location.hostname;
         const path = "/wc/join/" + window.location.pathname.substring(3);
         window.location.href = "https://" + domain + path;
       }
-    }
   });
 })();
