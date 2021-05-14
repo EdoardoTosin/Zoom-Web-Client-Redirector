8b69349092c9673d14cab8a88cd45d2c4a37eebc
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2021-05-14 08:33:33 +0200

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -1,49 +1,9 @@
-var browser = (window.browser)? window.browser : window.chrome;
-
-// Set redirect option on after install
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({'toggle': "true"});
-  chrome.browserAction.setIcon({
-      path: {
-        "16": "../icons/16x16.png",
-        "32": "../icons/32x32.png",
-        "48": "../icons/48x48.png",
-        "64": "../icons/64x64.png",
-        "128": "../icons/128x128.png",
-        "256": "../icons/256x256.png"
-      }
-  });
-});
-
-// When the popup page load it checks the stored option and change toggle and icon set.
-document.body.onload = function() {
-  chrome.storage.sync.get("toggle", function(items) {
-    if (items.toggle=="false")
-      setRedirectOff();
-    else {
-      setRedirectOn();
-    }
-  });
-}
-
-// If toggle change state it calls changeElem function.
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#toggle').addEventListener('change', saveOption);
-});
-
-// Change localStorage "toggleStatus" value and extension icon when checkbox change state.
-function saveOption() {
-  if (document.getElementById('toggle').checked){
-    setRedirectOn();
-  }
-  else{
-    setRedirectOff();
-  }
-};
+var storage = chrome.storage.sync;
+if (!storage) storage = chrome.storage.local;
 
 // Change Icon, Toggle and localStorage to true/on.
 function setRedirectOn(){
-  chrome.storage.sync.set({'toggle': "true"});
+  storage.set({'toggle': "true"});
   document.getElementById('toggle').setAttribute("checked","");
   chrome.browserAction.setIcon({
       path: {
@@ -59,7 +19,7 @@ function setRedirectOn(){
 
 // Change Icon, Toggle and localStorage to false/off.
 function setRedirectOff(){
-  chrome.storage.sync.set({'toggle': "false"});
+  storage.set({'toggle': "false"});
   document.getElementById('toggle').removeAttribute("checked");
   chrome.browserAction.setIcon({
       path: {
@@ -72,3 +32,38 @@ function setRedirectOff(){
       }
   });
 };
+
+// Set redirect option on after install
+chrome.runtime.onInstalled.addListener(function() {
+  setRedirectOn();
+});
+
+// When the popup page load it checks the stored option and change toggle and icon set.
+document.body.onload = function() {
+  storage.get("toggle", function(items) {
+    //console.log('Load to: ' + items.toggle);
+    if (items.toggle!='undefined' && items.toggle=="false")
+      setRedirectOff();
+    else {
+      setRedirectOn();
+    }
+  });
+}
+
+// Change localStorage "toggleStatus" value and extension icon when checkbox change state.
+function saveOption() {
+  if (document.getElementById('toggle').checked){
+    setRedirectOn();
+  }
+  else{
+    setRedirectOff();
+  }/*
+  storage.get("toggle", function(items) {
+    console.log('Set to: ' + items.toggle);
+  });*/
+};
+
+// If toggle change state it calls changeElem function.
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#toggle').addEventListener('change', saveOption);
+});
