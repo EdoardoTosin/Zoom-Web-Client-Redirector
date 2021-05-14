3814330b1deb0369dd3860a6e8f91a7bb88ac1be
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2021-05-14 08:32:43 +0200

    Update redirect.js

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -1,12 +1,14 @@
+var storage = chrome.storage.sync;
+if (!storage) storage = chrome.storage.local;
+
 // Replace */j/* on zoom domain with */wc/join/*
 (function redirect(){
-  chrome.storage.sync.get("toggle", function(items) {
-      if (!chrome.runtime.error) {
-        if (items.toggle=="true" && window.location.pathname != null && window.location.pathname.substring(0,3) == "/j/"){
-          const domain = window.location.hostname;
-          const path = "/wc/join/" + window.location.pathname.substring(3);
-          window.location.href = "https://" + domain + path;
-        }
+  storage.get("toggle", function(items) {
+      console.log(items.toggle);
+      if (items.toggle=="true" && window.location.pathname!=null && window.location.pathname.substring(0,3) == "/j/"){
+        var domain = window.location.hostname;
+        var path = "/wc/join/" + window.location.pathname.substring(3);
+        window.location.href = "https://" + domain + path;
       }
   });
 })();
