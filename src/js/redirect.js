35abb9bba13f96677977981606ce35b4eb42a26e
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2022-01-10 18:05:42 +0100

    #21 include query to new link

--- src/js/redirect.js
+++ src/js/redirect.js
@@ -7,7 +7,7 @@ if (!storage) storage = chrome.storage.local;
       //console.log(items.toggle);
       if (items.toggle=="true" && window.location.pathname!=null && window.location.pathname.substring(0,3).match(/[/]{1}[A-Za-z]{1}[/]{1}/)){
         var domain = window.location.hostname;
-        var path = "/wc/join/" + window.location.pathname.substring(3);
+        var path = "/wc/join/" + window.location.pathname.substring(3) + window.location.search;
         window.location.href = "https://" + domain + path;
       }
   });
